19f6f0bb76ba0f91582d14ee90476f6ace3746c3
lilblupig <lilblue2005@hotmail.co.uk>
2021-04-09 08:14:27 +0000

    JS - Define random number generator for operands.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Have game start only when page has loaded, then have game listen for input.
+
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
@@ -14,7 +16,13 @@ document.addEventListener("DOMContentLoaded", function() {
 })
 
 function runGame() {
+    // Generate two random numbers between 1 and 25
+    // Math.random generates random numbers between 0 and 1
+    // Math.floor rounds down to the whole number
+    // Generates numbers between 0 and 24 so add 1 (Use Math.ceiling instead?)
 
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
