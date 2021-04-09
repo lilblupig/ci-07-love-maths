6b638af93a8302ec9b774b1944dbcb71adaacb77
lilblupig <lilblue2005@hotmail.co.uk>
2021-04-09 10:03:37 +0000

    JS - Define multiply question.

--- script.js
+++ script.js
@@ -105,7 +105,11 @@ function displaySubtractionQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 
 }
 
